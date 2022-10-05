b4ce92e1f6300f59da5528aaa74ea85cd3b133de
Alex7-8 <71002189+Alex7-8@users.noreply.github.com>
2022-10-05 14:31:27 -0600

    V1

--- config/conexion.php
+++ config/conexion.php
@@ -4,14 +4,14 @@
 
         protected function Conexion(){
             try {
-				$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=chacongt_DBWeb","chacongt_alex56","alex56");
+				$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=andercode_webservice","root","");
 				return $conectar;	
-                
 			} catch (Exception $e) {
 				print "¡Error BD!: " . $e->getMessage() . "<br/>";
 				die();	
 			}
         }
+
         public function set_names(){	
 			return $this->dbh->query("SET NAMES 'utf8'");
         }
