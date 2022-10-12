da2773520a36918d120be66885ffe345c9fbc131
KatyMD <72051777+KatyMD@users.noreply.github.com>
2022-10-11 23:37:51 -0500

    Update katy.php
    categoria

--- katy.php
+++ katy.php
@@ -5,10 +5,50 @@ class katy{
 
 public function crear(){
 
-}
-public function editar(){
+public function insert_categoria($nombre,$descripcion,$estado){
+    $conectar= parent::conexion();
+    parent::set_names();
+    $sql="INSERT INTO articulos(id,nombre, descripcion,estado) VALUES (NULL,?,?,'1');";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1, $nombre);
+    $sql->bindValue(2, $descripcion);
+    $sql->bindValue(3, $estado);
+    $sql->execute();
+    return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
 }
 
+
+public function update_categoria($id,$nombre,$descripcion,$estado){
+    $conectar= parent::conexion();
+    parent::set_names();
+    $sql="UPDATE articulos set
+        nombre = ?,
+        descripcion = ?,
+        estado = ?
+        WHERE 
+        id = ?
+        ";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1, $nombre);
+    $sql->bindValue(2, $descripcion);
+    $sql->bindValue(3, $estado);
+    $sql->bindValue(4, $id);
+    $sql->execute();
+ return $resultado=$sql->fetchAll();
+}
+
+public function delete_articulos($id){
+    $conectar= parent::conexion();
+    parent::set_names();
+    $sql="UPDATE categoria set
+        estado = '0'
+        WHERE
+        id = ?";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1, $id);
+    $sql->execute();
+    return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+}
 
 
 
