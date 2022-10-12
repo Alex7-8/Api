474e1389de5e8feaaccf9d413ec1c94efd6935f5
KatyMD <72051777+KatyMD@users.noreply.github.com>
2022-10-11 23:58:21 -0500

    categoria

--- katy.php
+++ katy.php
@@ -3,7 +3,24 @@ class katy{
 
 
 
-public function crear(){
+public function get_categoria(){
+    $conectar= parent::conexion();
+    parent::set_names();
+    $sql="SELECT * FROM categoria WHERE estado = 'Activo' ";
+    $sql=$conectar->prepare($sql);
+    $sql->execute();
+    return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+}
+
+public function get_categoria_id($id){
+    $conectar= parent::conexion();
+    parent::set_names();
+    $sql="SELECT * FROM categoria WHERE id = ?";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1, $id);
+    $sql->execute();
+    return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+}
 
 public function insert_categoria($nombre,$descripcion,$estado){
     $conectar= parent::conexion();
@@ -37,7 +54,7 @@ public function update_categoria($id,$nombre,$descripcion,$estado){
  return $resultado=$sql->fetchAll();
 }
 
-public function delete_articulos($id){
+public function delete_categoria($id){
     $conectar= parent::conexion();
     parent::set_names();
     $sql="UPDATE categoria set
